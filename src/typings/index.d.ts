8748207415295fde5151d2f2e4899118940f5b21
rafacalderon <rafacalderon1297@gmail.com>
2023-04-30 23:13:12 -0400

    fix: typings

--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -1,10 +1,14 @@
 import type {App} from "vue";
-
 import {IVGoogleMap} from "@/typings/vGoogleMap";
+import type {useGmapLoader as GmapLoader} from "@/composables/gmapLoader";
 
 export declare interface VGoogleMaps {
   install(app: App): void;
 }
+
+export declare function useGmapLoader(): typeof GmapLoader;
+
+export declare function vGoogleMaps(): VGoogleMaps;
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
