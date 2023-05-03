32c6d08ec844c0892293dc499e67bde79f2acdbf
Rafael <rafacalderon1297@gmail.com>
2023-05-03 13:47:02 -0400

    feat: arreglo typings

--- src/typings/vGoogleInfoWindow.d.ts
+++ src/typings/vGoogleInfoWindow.d.ts
@@ -1,7 +1,7 @@
 import type {AllowedComponentProps, ComponentCustomProps, VNodeProps} from "vue";
 
 export declare interface VGoogleInfoWindowProps {
-  options: google.maps.InfoWindowOptions;
+  options?: google.maps.InfoWindowOptions;
   modelValue?: boolean | null | undefined;
   "@click"?: (ev: google.maps.MapMouseEvent) => void;
   "@update:model-value"?: (value: boolean | null) => void;
