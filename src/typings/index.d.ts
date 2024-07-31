dc82e73a959d6984afdf823d94692c46fdc70229
rafacalderon <rafacalderon1297@gmail.com>
2024-07-31 10:13:23 -0400

    feat: types

--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -1,5 +1,5 @@
-import type { App, Ref } from "vue";
-import type { Libraries, Loader } from "@googlemaps/js-api-loader";
+import type { App } from "vue";
+import type GoogleMapsLoader from "./composables/googleMapsLoader";
 import type { MarkerClustererOptions } from "@googlemaps/markerclusterer";
 
 import type VGoogleMap from "./components/VGoogleMap.vue";
@@ -16,13 +16,7 @@ export declare interface VGoogleMaps {
   install(app: App): void;
 }
 
-export declare function useGoogleMapsLoader(): {
-  loader: Ref<Loader | null>;
-  maps: Ref<google.maps.MapsLibrary | null>;
-  markers: Ref<google.maps.MarkerLibrary | null>;
-  visualization: Ref<google.maps.VisualizationLibrary | null>;
-  init: (apiKey: string, libraries: Libraries = []) => Promise<void>;
-};
+export type { GoogleMapsLoader };
 
 export declare function vGoogleMaps(): VGoogleMaps;
 
