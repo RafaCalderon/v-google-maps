102f77ee3560397290f2e9c4d45e8d59c387ff5d
rafacalderon <rafacalderon1297@gmail.com>
2024-11-05 15:17:03 -0300

    feat: types

--- types/global-components.d.ts
+++ types/global-components.d.ts
@@ -1,11 +1,5 @@
 import { VNodeProps, ComponentCustomProps, AllowedComponentProps } from "vue";
 
-declare module "*.vue" {
-  import { defineComponent } from "vue";
-  const component: ReturnType<typeof defineComponent>;
-  export default component;
-}
-
 declare const VGooglePolyline: new () => {
   $props: AllowedComponentProps & ComponentCustomProps & VNodeProps;
 };
