26b514274ec4e22e01ff70ba2e86f0abef79034b
rafacalderon <rafacalderon1297@gmail.com>
2025-02-05 11:54:15 -0300

    feat: format

--- src/typings/components/VGoogleRectangle.d.ts
+++ src/typings/components/VGoogleRectangle.d.ts
@@ -1,28 +1,58 @@
 import { type PropType } from "vue";
-declare const _default: import("vue").DefineComponent<import("vue").ExtractPropTypes<{
+declare const _default: import("vue").DefineComponent<
+  import("vue").ExtractPropTypes<{
     options: {
-        required: true;
-        type: PropType<google.maps.RectangleOptions>;
+      required: true;
+      type: PropType<google.maps.RectangleOptions>;
     };
     modelValue: {
-        default: null;
-        type: PropType<google.maps.LatLngBoundsLiteral | null>;
+      default: null;
+      type: PropType<google.maps.LatLngBoundsLiteral | null>;
     };
-}>, () => import("vue").VNode<import("vue").RendererNode, import("vue").RendererElement, {
-    [key: string]: any;
-}>[] | undefined, {}, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, ("click" | "update:model-value")[], "click" | "update:model-value", import("vue").PublicProps, Readonly<import("vue").ExtractPropTypes<{
-    options: {
+  }>,
+  () =>
+    | import("vue").VNode<
+        import("vue").RendererNode,
+        import("vue").RendererElement,
+        {
+          [key: string]: any;
+        }
+      >[]
+    | undefined,
+  {},
+  {},
+  {},
+  import("vue").ComponentOptionsMixin,
+  import("vue").ComponentOptionsMixin,
+  ("click" | "update:model-value")[],
+  "click" | "update:model-value",
+  import("vue").PublicProps,
+  Readonly<
+    import("vue").ExtractPropTypes<{
+      options: {
         required: true;
         type: PropType<google.maps.RectangleOptions>;
-    };
-    modelValue: {
+      };
+      modelValue: {
         default: null;
         type: PropType<google.maps.LatLngBoundsLiteral | null>;
-    };
-}>> & Readonly<{
-    onClick?: ((...args: any[]) => any) | undefined;
-    "onUpdate:model-value"?: ((...args: any[]) => any) | undefined;
-}>, {
+      };
+    }>
+  > &
+    Readonly<{
+      onClick?: ((...args: any[]) => any) | undefined;
+      "onUpdate:model-value"?: ((...args: any[]) => any) | undefined;
+    }>,
+  {
     modelValue: google.maps.LatLngBoundsLiteral | null;
-}, {}, {}, {}, string, import("vue").ComponentProvideOptions, true, {}, any>;
+  },
+  {},
+  {},
+  {},
+  string,
+  import("vue").ComponentProvideOptions,
+  true,
+  {},
+  any
+>;
 export default _default;
