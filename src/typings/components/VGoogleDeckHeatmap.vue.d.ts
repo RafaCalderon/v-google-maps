4486373301914fdb27ceb9cc0e53eae443146e62
Rafael <rafacalderon1297@gmail.com>
2025-11-10 18:05:56 -0300

    feat: format

--- src/typings/components/VGoogleDeckHeatmap.vue.d.ts
+++ src/typings/components/VGoogleDeckHeatmap.vue.d.ts
@@ -1,78 +1,90 @@
 import { GoogleMapsOverlay } from "@deck.gl/google-maps";
 import { type HeatmapLayerProps } from "@deck.gl/aggregation-layers";
-declare const __VLS_export: <T>(__VLS_props: NonNullable<Awaited<typeof __VLS_setup>>["props"], __VLS_ctx?: __VLS_PrettifyLocal<Pick<NonNullable<Awaited<typeof __VLS_setup>>, "attrs" | "emit" | "slots">>, __VLS_expose?: NonNullable<Awaited<typeof __VLS_setup>>["expose"], __VLS_setup?: Promise<{
+declare const __VLS_export: <T>(
+  __VLS_props: NonNullable<Awaited<typeof __VLS_setup>>["props"],
+  __VLS_ctx?: __VLS_PrettifyLocal<
+    Pick<NonNullable<Awaited<typeof __VLS_setup>>, "attrs" | "emit" | "slots">
+  >,
+  __VLS_expose?: NonNullable<Awaited<typeof __VLS_setup>>["expose"],
+  __VLS_setup?: Promise<{
     props: __VLS_PrettifyLocal<{
-        options: HeatmapLayerProps<T>;
-    }> & import("vue").PublicProps;
-    expose: (exposed: import("vue").ShallowUnwrapRef<{
-        overlay: import("vue").Ref<{
+      options: HeatmapLayerProps<T>;
+    }> &
+      import("vue").PublicProps;
+    expose: (
+      exposed: import("vue").ShallowUnwrapRef<{
+        overlay: import("vue").Ref<
+          {
             setMap: (map: google.maps.Map | null) => void;
-            setProps: (props: Partial<import("@deck.gl/google-maps").GoogleMapsOverlayProps>) => void;
+            setProps: (
+              props: Partial<import("@deck.gl/google-maps").GoogleMapsOverlayProps>,
+            ) => void;
             pickObject: (params: any) => {
-                color: Uint8Array | null;
-                layer: import("@deck.gl/core").Layer | null;
-                sourceLayer?: import("@deck.gl/core").Layer | null;
-                viewport?: import("@deck.gl/core").Viewport;
-                index: number;
-                picked: boolean;
-                object?: any;
-                x: number;
-                y: number;
-                pixel?: [number, number];
-                coordinate?: number[];
-                devicePixel?: [number, number];
-                pixelRatio: number;
+              color: Uint8Array | null;
+              layer: import("@deck.gl/core").Layer | null;
+              sourceLayer?: import("@deck.gl/core").Layer | null;
+              viewport?: import("@deck.gl/core").Viewport;
+              index: number;
+              picked: boolean;
+              object?: any;
+              x: number;
+              y: number;
+              pixel?: [number, number];
+              coordinate?: number[];
+              devicePixel?: [number, number];
+              pixelRatio: number;
             } | null;
-            pickMultipleObjects: (params: any) => {
-                color: Uint8Array | null;
-                layer: import("@deck.gl/core").Layer | null;
-                sourceLayer?: import("@deck.gl/core").Layer | null;
-                viewport?: import("@deck.gl/core").Viewport;
-                index: number;
-                picked: boolean;
-                object?: any;
-                x: number;
-                y: number;
-                pixel?: [number, number];
-                coordinate?: number[];
-                devicePixel?: [number, number];
-                pixelRatio: number;
-            }[] | null;
-            pickObjects: (params: any) => {
-                color: Uint8Array | null;
-                layer: import("@deck.gl/core").Layer | null;
-                sourceLayer?: import("@deck.gl/core").Layer | null;
-                viewport?: import("@deck.gl/core").Viewport;
-                index: number;
-                picked: boolean;
-                object?: any;
-                x: number;
-                y: number;
-                pixel?: [number, number];
-                coordinate?: number[];
-                devicePixel?: [number, number];
-                pixelRatio: number;
-            }[] | null;
+            pickMultipleObjects: (params: any) =>
+              | {
+                  color: Uint8Array | null;
+                  layer: import("@deck.gl/core").Layer | null;
+                  sourceLayer?: import("@deck.gl/core").Layer | null;
+                  viewport?: import("@deck.gl/core").Viewport;
+                  index: number;
+                  picked: boolean;
+                  object?: any;
+                  x: number;
+                  y: number;
+                  pixel?: [number, number];
+                  coordinate?: number[];
+                  devicePixel?: [number, number];
+                  pixelRatio: number;
+                }[]
+              | null;
+            pickObjects: (params: any) =>
+              | {
+                  color: Uint8Array | null;
+                  layer: import("@deck.gl/core").Layer | null;
+                  sourceLayer?: import("@deck.gl/core").Layer | null;
+                  viewport?: import("@deck.gl/core").Viewport;
+                  index: number;
+                  picked: boolean;
+                  object?: any;
+                  x: number;
+                  y: number;
+                  pixel?: [number, number];
+                  coordinate?: number[];
+                  devicePixel?: [number, number];
+                  pixelRatio: number;
+                }[]
+              | null;
             finalize: () => void;
             _createOverlay: (map: google.maps.Map) => void;
             _onAdd: () => void;
-            _onContextRestored: ({ gl }: {
-                gl: any;
-            }) => void;
+            _onContextRestored: ({ gl }: { gl: any }) => void;
             _onContextLost: () => void;
             _onRemove: () => void;
             _onDrawRaster: () => void;
-            _onDrawVectorInterleaved: ({ gl, transformer }: {
-                gl: any;
-                transformer: any;
-            }) => void;
-            _onDrawVectorOverlay: ({ transformer }: {
-                transformer: any;
-            }) => void;
-        } | null, GoogleMapsOverlay | {
-            setMap: (map: google.maps.Map | null) => void;
-            setProps: (props: Partial<import("@deck.gl/google-maps").GoogleMapsOverlayProps>) => void;
-            pickObject: (params: any) => {
+            _onDrawVectorInterleaved: ({ gl, transformer }: { gl: any; transformer: any }) => void;
+            _onDrawVectorOverlay: ({ transformer }: { transformer: any }) => void;
+          } | null,
+          | GoogleMapsOverlay
+          | {
+              setMap: (map: google.maps.Map | null) => void;
+              setProps: (
+                props: Partial<import("@deck.gl/google-maps").GoogleMapsOverlayProps>,
+              ) => void;
+              pickObject: (params: any) => {
                 color: Uint8Array | null;
                 layer: import("@deck.gl/core").Layer | null;
                 sourceLayer?: import("@deck.gl/core").Layer | null;
@@ -86,65 +98,72 @@ declare const __VLS_export: <T>(__VLS_props: NonNullable<Awaited<typeof __VLS_se
                 coordinate?: number[];
                 devicePixel?: [number, number];
                 pixelRatio: number;
-            } | null;
-            pickMultipleObjects: (params: any) => {
-                color: Uint8Array | null;
-                layer: import("@deck.gl/core").Layer | null;
-                sourceLayer?: import("@deck.gl/core").Layer | null;
-                viewport?: import("@deck.gl/core").Viewport;
-                index: number;
-                picked: boolean;
-                object?: any;
-                x: number;
-                y: number;
-                pixel?: [number, number];
-                coordinate?: number[];
-                devicePixel?: [number, number];
-                pixelRatio: number;
-            }[] | null;
-            pickObjects: (params: any) => {
-                color: Uint8Array | null;
-                layer: import("@deck.gl/core").Layer | null;
-                sourceLayer?: import("@deck.gl/core").Layer | null;
-                viewport?: import("@deck.gl/core").Viewport;
-                index: number;
-                picked: boolean;
-                object?: any;
-                x: number;
-                y: number;
-                pixel?: [number, number];
-                coordinate?: number[];
-                devicePixel?: [number, number];
-                pixelRatio: number;
-            }[] | null;
-            finalize: () => void;
-            _createOverlay: (map: google.maps.Map) => void;
-            _onAdd: () => void;
-            _onContextRestored: ({ gl }: {
-                gl: any;
-            }) => void;
-            _onContextLost: () => void;
-            _onRemove: () => void;
-            _onDrawRaster: () => void;
-            _onDrawVectorInterleaved: ({ gl, transformer }: {
+              } | null;
+              pickMultipleObjects: (params: any) =>
+                | {
+                    color: Uint8Array | null;
+                    layer: import("@deck.gl/core").Layer | null;
+                    sourceLayer?: import("@deck.gl/core").Layer | null;
+                    viewport?: import("@deck.gl/core").Viewport;
+                    index: number;
+                    picked: boolean;
+                    object?: any;
+                    x: number;
+                    y: number;
+                    pixel?: [number, number];
+                    coordinate?: number[];
+                    devicePixel?: [number, number];
+                    pixelRatio: number;
+                  }[]
+                | null;
+              pickObjects: (params: any) =>
+                | {
+                    color: Uint8Array | null;
+                    layer: import("@deck.gl/core").Layer | null;
+                    sourceLayer?: import("@deck.gl/core").Layer | null;
+                    viewport?: import("@deck.gl/core").Viewport;
+                    index: number;
+                    picked: boolean;
+                    object?: any;
+                    x: number;
+                    y: number;
+                    pixel?: [number, number];
+                    coordinate?: number[];
+                    devicePixel?: [number, number];
+                    pixelRatio: number;
+                  }[]
+                | null;
+              finalize: () => void;
+              _createOverlay: (map: google.maps.Map) => void;
+              _onAdd: () => void;
+              _onContextRestored: ({ gl }: { gl: any }) => void;
+              _onContextLost: () => void;
+              _onRemove: () => void;
+              _onDrawRaster: () => void;
+              _onDrawVectorInterleaved: ({
+                gl,
+                transformer,
+              }: {
                 gl: any;
                 transformer: any;
-            }) => void;
-            _onDrawVectorOverlay: ({ transformer }: {
-                transformer: any;
-            }) => void;
-        } | null>;
-    }>) => void;
+              }) => void;
+              _onDrawVectorOverlay: ({ transformer }: { transformer: any }) => void;
+            }
+          | null
+        >;
+      }>,
+    ) => void;
     attrs: any;
     slots: {
-        default?: (props: {}) => any;
+      default?: (props: {}) => any;
     };
     emit: {};
-}>) => import("vue").VNode & {
-    __ctx?: Awaited<typeof __VLS_setup>;
+  }>,
+) => import("vue").VNode & {
+  __ctx?: Awaited<typeof __VLS_setup>;
 };
 declare const _default: typeof __VLS_export;
 export default _default;
 type __VLS_PrettifyLocal<T> = {
-    [K in keyof T as K]: T[K];
+  [K in keyof T as K]: T[K];
 } & {};
--- src/typings/components/VGoogleInfoWindow.vue.d.ts
+++ src/typings/components/VGoogleInfoWindow.vue.d.ts
@@ -1,17 +1,20 @@
 interface Props {
-    options?: google.maps.InfoWindowOptions | null;
+  options?: google.maps.InfoWindowOptions | null;
 }
 type __VLS_Props = Props;
 type __VLS_ModelProps = {
-    modelValue?: boolean;
+  modelValue?: boolean;
 };
 type __VLS_PublicProps = __VLS_Props & __VLS_ModelProps;
 declare var __VLS_1: {};
 type __VLS_Slots = {} & {
-    default?: (props: typeof __VLS_1) => any;
+  default?: (props: typeof __VLS_1) => any;
 };
-declare const __VLS_base: import("vue").DefineComponent<__VLS_PublicProps, {
-    infoWindow: import("vue").Ref<{
+declare const __VLS_base: import("vue").DefineComponent<
+  __VLS_PublicProps,
+  {
+    infoWindow: import("vue").Ref<
+      {
         close: () => void;
         focus: () => void;
         getContent: () => string | Element | null | Text | undefined;
@@ -20,7 +23,14 @@ declare const __VLS_base: import("vue").DefineComponent<__VLS_PublicProps, {
         getPosition: () => google.maps.LatLng | null | undefined;
         getZIndex: () => number;
         isOpen: boolean;
-        open: (options?: google.maps.InfoWindowOpenOptions | null | google.maps.Map | google.maps.StreetViewPanorama, anchor?: google.maps.MVCObject | null | google.maps.marker.AdvancedMarkerElement) => void;
+        open: (
+          options?:
+            | google.maps.InfoWindowOpenOptions
+            | null
+            | google.maps.Map
+            | google.maps.StreetViewPanorama,
+          anchor?: google.maps.MVCObject | null | google.maps.marker.AdvancedMarkerElement,
+        ) => void;
         setContent: (content?: string | Element | null | Text) => void;
         setHeaderContent: (headerContent?: string | Element | null | Text) => void;
         setHeaderDisabled: (headerDisabled?: boolean | null) => void;
@@ -28,50 +38,91 @@ declare const __VLS_base: import("vue").DefineComponent<__VLS_PublicProps, {
         setPosition: (position?: google.maps.LatLng | null | google.maps.LatLngLiteral) => void;
         setZIndex: (zIndex: number) => void;
         addListener: (eventName: string, handler: Function) => google.maps.MapsEventListener;
-        bindTo: (key: string, target: google.maps.MVCObject, targetKey?: string | null, noNotify?: boolean) => void;
+        bindTo: (
+          key: string,
+          target: google.maps.MVCObject,
+          targetKey?: string | null,
+          noNotify?: boolean,
+        ) => void;
         get: (key: string) => any;
         notify: (key: string) => void;
         set: (key: string, value: unknown) => void;
         setValues: (values?: object | null) => void;
         unbind: (key: string) => void;
         unbindAll: () => void;
-    } | null, google.maps.InfoWindow | {
-        close: () => void;
-        focus: () => void;
-        getContent: () => string | Element | null | Text | undefined;
-        getHeaderContent: () => string | Element | null | Text | undefined;
-        getHeaderDisabled: () => boolean | undefined;
-        getPosition: () => google.maps.LatLng | null | undefined;
-        getZIndex: () => number;
-        isOpen: boolean;
-        open: (options?: google.maps.InfoWindowOpenOptions | null | google.maps.Map | google.maps.StreetViewPanorama, anchor?: google.maps.MVCObject | null | google.maps.marker.AdvancedMarkerElement) => void;
-        setContent: (content?: string | Element | null | Text) => void;
-        setHeaderContent: (headerContent?: string | Element | null | Text) => void;
-        setHeaderDisabled: (headerDisabled?: boolean | null) => void;
-        setOptions: (options?: google.maps.InfoWindowOptions | null) => void;
-        setPosition: (position?: google.maps.LatLng | null | google.maps.LatLngLiteral) => void;
-        setZIndex: (zIndex: number) => void;
-        addListener: (eventName: string, handler: Function) => google.maps.MapsEventListener;
-        bindTo: (key: string, target: google.maps.MVCObject, targetKey?: string | null, noNotify?: boolean) => void;
-        get: (key: string) => any;
-        notify: (key: string) => void;
-        set: (key: string, value: unknown) => void;
-        setValues: (values?: object | null) => void;
-        unbind: (key: string) => void;
-        unbindAll: () => void;
-    } | null>;
-}, {}, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {
+      } | null,
+      | google.maps.InfoWindow
+      | {
+          close: () => void;
+          focus: () => void;
+          getContent: () => string | Element | null | Text | undefined;
+          getHeaderContent: () => string | Element | null | Text | undefined;
+          getHeaderDisabled: () => boolean | undefined;
+          getPosition: () => google.maps.LatLng | null | undefined;
+          getZIndex: () => number;
+          isOpen: boolean;
+          open: (
+            options?:
+              | google.maps.InfoWindowOpenOptions
+              | null
+              | google.maps.Map
+              | google.maps.StreetViewPanorama,
+            anchor?: google.maps.MVCObject | null | google.maps.marker.AdvancedMarkerElement,
+          ) => void;
+          setContent: (content?: string | Element | null | Text) => void;
+          setHeaderContent: (headerContent?: string | Element | null | Text) => void;
+          setHeaderDisabled: (headerDisabled?: boolean | null) => void;
+          setOptions: (options?: google.maps.InfoWindowOptions | null) => void;
+          setPosition: (position?: google.maps.LatLng | null | google.maps.LatLngLiteral) => void;
+          setZIndex: (zIndex: number) => void;
+          addListener: (eventName: string, handler: Function) => google.maps.MapsEventListener;
+          bindTo: (
+            key: string,
+            target: google.maps.MVCObject,
+            targetKey?: string | null,
+            noNotify?: boolean,
+          ) => void;
+          get: (key: string) => any;
+          notify: (key: string) => void;
+          set: (key: string, value: unknown) => void;
+          setValues: (values?: object | null) => void;
+          unbind: (key: string) => void;
+          unbindAll: () => void;
+        }
+      | null
+    >;
+  },
+  {},
+  {},
+  {},
+  import("vue").ComponentOptionsMixin,
+  import("vue").ComponentOptionsMixin,
+  {
     "update:modelValue": (value: boolean) => any;
-}, string, import("vue").PublicProps, Readonly<__VLS_PublicProps> & Readonly<{
-    "onUpdate:modelValue"?: ((value: boolean) => any) | undefined;
-}>, {
+  },
+  string,
+  import("vue").PublicProps,
+  Readonly<__VLS_PublicProps> &
+    Readonly<{
+      "onUpdate:modelValue"?: ((value: boolean) => any) | undefined;
+    }>,
+  {
     options: google.maps.InfoWindowOptions | null;
-}, {}, {}, {}, string, import("vue").ComponentProvideOptions, false, {}, any>;
+  },
+  {},
+  {},
+  {},
+  string,
+  import("vue").ComponentProvideOptions,
+  false,
+  {},
+  any
+>;
 declare const __VLS_export: __VLS_WithSlots<typeof __VLS_base, __VLS_Slots>;
 declare const _default: typeof __VLS_export;
 export default _default;
 type __VLS_WithSlots<T, S> = T & {
-    new (): {
-        $slots: S;
-    };
+  new (): {
+    $slots: S;
+  };
 };
